b756097c72a9eccc961857ab343e169ebea09c0c
thakurmihir <mihirthakur.cse@gmail.com>
2017-05-10 08:38:20 +0530

    vplayer added

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer'
+gem 'angularjs-rails'
 gem 'middleman-sprockets'
 gem 'bootstrap-sass', '~> 3.3.6'
-gem "jquery-middleman"
+
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,37 @@
-//= require jquery
-//= require bootstrap-sprockets
+//= require angular
+//= require_tree .
+
+
+
+var app = angular.module('myApp', []);
+
+app.config([
+  '$interpolateProvider', function($interpolateProvider) {
+    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+  }
+]);
+
+app.directive("aplayer", function() {
+  return {
+    restrict : "AC",
+    link: function(scope, element, attrs) {
+      var nativeElement = element[0];
+      var ap1 = new APlayer({
+        element: nativeElement,
+        narrow: false,
+        autoplay: true,
+        showlrc: false,
+        mutex: true,
+        theme: '#e6d0b2',
+        preload: 'metadata',
+        mode: 'circulation',
+        music: {
+          title: attrs["playerTitle"],
+          author: attrs["playerAuthor"],
+          url: attrs["playerUrl"],
+          pic: attrs["playerPic"]
+        }
+      });
+    }
+  };
+});
