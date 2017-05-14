bc475d709851d05a7f35d136d662a1ab7218fff6
thakurmihir <mihirthakur.cse@gmail.com>
2017-05-14 07:14:22 +0530

    test button

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,27 +9,8 @@ app.config([
   }
 ]);
 
-app.directive("aplayer", function() {
-  return {
-    restrict : "AC",
-    link: function(scope, element, attrs) {
-      var nativeElement = element[0];
-      var ap1 = new APlayer({
-        element: nativeElement,
-        narrow: false,
-        autoplay: false,
-        showlrc: false,
-        mutex: true,
-        theme: '#ad7a86',
-        mode: 'circulation',
-        listmaxheight: '0px',
-        music:[
-        {
-            title: 'あっちゅ～ま青春!',
-            author: '七森中☆ごらく部',
-            url: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.mp3',
-            pic: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg'
-        },
+app.controller('TestCtrl', function($scope) {
+  $scope.musics = [
         {
             title: 'secret base~君がくれたもの~',
             author: '茅野愛衣',
@@ -42,8 +23,56 @@ app.directive("aplayer", function() {
             url: 'http://devtest.qiniudn.com/回レ！雪月花.mp3',
             pic: 'http://devtest.qiniudn.com/回レ！雪月花.jpg'
         }
-    ]
+    ];
+}).directive("aplayer", function() {
+  return {
+    restrict : "AC",
+    controller: function($scope) {
+      this.setValue = function(value) {
+        $scope.value = value;
+      },
+      $scope.ngFn = function () {
+        console.log("ngFn is triggered!");
+        var current_song_no = angular.element(document.querySelectorAll("ol li")).length;
+        var player = $scope.value;
+        player.addMusic([{
+          title: 'Mihir TestCtrl',
+          author: 'Mihir Hello',
+          url: 'https://wynk-play.akamaized.net/srch_unisysinfo/music/64/1493270976/srch_unisysinfo_M0904771.mp3?token=1494727952_4cc9a4a5149708725b717a1efb526154',
+          pic: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg'
+        }])
+        player.setMusic(current_song_no);
+      }
+    },
+    link: function(scope, element, attrs, ctrl) {
+      var nativeElement = element[0];
+      var music = scope.musics;
+      var ap1 = new APlayer({
+        element: nativeElement,
+        narrow: false,
+        autoplay: false,
+        showlrc: false,
+        mutex: true,
+        theme: '#ad7a86',
+        mode: 'circulation',
+        listmaxheight: '0px',
+        music: music
       });
+      ctrl.setValue(ap1);
     }
   };
 });
+
+// app.directive('foo', function() {
+//   return {
+//     scope: {},
+//     controller: function($scope) {
+//       this.setValue = function(value) {
+//         console.log("setting value: " + value);
+//       };
+//     },
+//     link: function(scope, element, attr, ctrl) {
+//       ctrl.setValue('foo');
+//     }
+//   };
+// });
