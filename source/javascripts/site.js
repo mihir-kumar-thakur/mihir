3bfae675acbe66afb6d6cb5a1722933f0e4e5d44
thakurmihir <mihirthakur.cse@gmail.com>
2017-05-28 18:39:38 +0530

    website_design_first_iteration

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,13 @@
+//= require jquery
+//= require bootstrap
+//= require jquery_flexslider
 //= require angular
+//= require angular-flexslider
 //= require_tree .
 
 (function() {
   'use strict';
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['angular-flexslider']);
 
 app.config([
   '$interpolateProvider', function($interpolateProvider) {
@@ -60,6 +64,22 @@ app.controller("TestCtrl", ["$scope", function ($scope) {
       ctrl.setValue(ap1);
     }
   };
+}]);
+
+app.controller('AlbumsController', ["$scope", function ($scope) {
+  $scope.slides = [
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg',
+    'https://www.planwallpaper.com/static/cache/ed/77/ed77608a0de73dc372295d292ba94096.jpg',
+    'https://www.planwallpaper.com/static/cache/00/b7/00b7a21d94164cb8764457a894063606.jpg',
+    'https://www.planwallpaper.com/static/cache/a1/4e/a14e880ef245c3d159ba96ebbeb4c8c3.jpg',
+    'https://www.planwallpaper.com/static/cache/e2/2f/e22fb97e62c21f7205c0ffff498aae4c.jpg',
+    'https://www.planwallpaper.com/static/cache/b3/57/b357b668312d2f875419b72a7f35996a.jpg',
+    'https://www.planwallpaper.com/static/cache/b0/a4/b0a46365aeba2ea94e557e9f3da91a07.jpg',
+    'https://www.planwallpaper.com/static/cache/4e/4f/4e4ffcdb4e1cd3b42a65db2bb209c910.jpg'
+  ];
 }]);
 
 })();
